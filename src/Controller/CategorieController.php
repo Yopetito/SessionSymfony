1a242229d0228b648699cd75e44df4cb69d3398f
yopetito <yofer.frg@gmail.com>
2025-01-21 15:51:24 +0100

    Formulaire ajouter un module

--- src/Controller/CategorieController.php
+++ src/Controller/CategorieController.php
@@ -2,8 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\Module;
+use App\Form\ModuleType;
 use App\Repository\ModuleRepository;
 use App\Repository\CategorieRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -20,14 +24,38 @@ final class CategorieController extends AbstractController
         ]);
     }
 
-    #[Route('/modules/{id}', name: 'app_modules')]
-    public function modulesInCategorie(int $id, ModuleRepository $moduleRepository): Response
+    
+    #[Route('/module/add', name: 'add_module')]
+    public function addModule(Module $module = null, ModuleRepository $moduleRepository, Request $request, EntityManagerInterface $entityManager): Response
     {
-        $modules = $moduleRepository->findBy(['categorie' => $id]);
+        $form = $this->createForm(ModuleType::class, $module);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $module = $form->getData();
+            $entityManager->persist($module);
+            $entityManager->flush();
 
-        return $this->render('module/index.html.twig', [
-            'modules' => $modules
+            return $this->redirectToRoute('app_categorie');
+        }
+
+        return $this->render('module/addmodule.html.twig', [
+            'formAddModule' => $form
         ]);
     }
+
+    #[Route('/detailsCategorie/{id}', name: 'app_modules')]
+    public function modulesInCategorie(CategorieRepository $categorieRepository,int $id, ModuleRepository $moduleRepository): Response
+    {
+
+        $categorie = $categorieRepository->find($id);
+
+        $modules = $moduleRepository->findBy(['categorie' => $id]);
+        return $this->render('module/index.html.twig', [
+            'modules' => $modules,
+            'categorie' => $categorie
+        ]);
+    }
+
+
 
 }
